e7d67c43c92eb2a9520fa8d08588fd14d5bfbc7c
Emily Barranca <emily@Emilys-MacBook-Air-8.local>
2025-09-25 11:02:14 -0400

    added 1D pilots

--- demo.py
+++ demo.py
@@ -2,7 +2,8 @@
 Adding code here to help give Olivia demos for basic python operations
 """
 
-
+def file_parse_practice(filename):
+    pass 
 
 if __name__ == "__main__":
     print("hello world")
