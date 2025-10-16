7ceea7dce7907f5f257713cbb80d158f9dcc708f
Emily Barranca <emily@Emilys-MacBook-Air-8.local>
2025-10-16 11:42:33 -0400

    weekly assignment

---
+++ graph.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    H = [1,3,7]
+    L = [4,6]
+    Li = [1,4,6]
+    Z = [1,2,5]
+    N = [2,7]
+
+    #goal: set up the graph for when pilots are free without hard coding to dummy data 
+    # \come up with a data structure to store pilot's last flight --maybe a pilot is an object 
